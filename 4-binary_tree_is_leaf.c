a28d431ebe0f36e9a5621a9103abd60db386993f
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-08-31 17:13:55 +0100

    rmoved an if statement from task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,12 +10,9 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->left == NULL && node->right == NULL)
+	if (node->left == NULL && node->right == NULL && node->parent != NULL)
 	{
-		if (node->parent != NULL)
-		{
 			return (1);
-		}
 	}
 
 	return (0);
