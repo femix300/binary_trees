24478005faef4e2b04845e008432926a1fc1c4e7
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-08-31 18:29:22 +0100

    removed the space at the start of line 14 in task 7 and added task 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,13 +4,13 @@
  * binary_tree_inorder - Traverses a binary tree using inorder method
  * @tree: pointer to the root node of the tree that is to be traversed
  * @func: pointer to a function call for each node
-*/
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
 
 	binary_tree_inorder(tree->left, func);
-    func(tree->n);
+	func(tree->n);
 	binary_tree_inorder(tree->right, func);
 }
