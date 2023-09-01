fae50de8e5fa790a79194b5ca08add646b7d4b74
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-01 01:42:11 +0100

    removed unnecessary curly braces from 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -30,7 +30,7 @@ int binary_tree_is_right(const binary_tree_t *node)
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to the node to find the sibling
  * Return: A pointer to the sibling node and null otherwise
-*/
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
@@ -41,16 +41,13 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (binary_tree_is_left(node))
 	{
 		if (node->parent->right != NULL)
-		{
 			return (node->parent->right);
-		}
 	}
+
 	if (binary_tree_is_right(node))
 	{
 		if (node->parent->left != NULL)
-		{
 			return (node->parent->left);
-		}
 	}
 
 	return (NULL);
