6652618cf7887e30a0f581ed7309e37e63a6d717
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-01 01:16:41 +0100

    added a helper function for task 16 and deleted some executables

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Measures the height of a binary tree(helper function).
+ * @tree: A pointer to the root node of the tree to measure the height.
+ * Return: The height of the tree or 0 if the tree is null.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  * Return: 1 if the tree is a perfect binary tree and null otherwise
